a5f4a68fdf534ad968a0dd59b5d0a17cb8c5eaea
heke9 <nikita.tihomirow768@.gmail.com>
2022-09-22 09:21:03 +0300

    123

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstname = "Nikita"
+let lastname = "Tihomirow"
+var age = 16
+if(age == 16){
+    //какое-то действие
+} else if(firstname == "Nikita"){
+    //какое-то действие
+}else{
+    //какое-то действие
+}console.log(firstname,lastname,age)
